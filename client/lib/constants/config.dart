b040b9327eccf8eda2d752d2f2e818aab7a01e31
Tauseeb Ahmed Siddiqui <122994540+Sid-Tauseef@users.noreply.github.com>
2025-03-02 14:42:58 +0530

    Update config.dart

--- client/lib/constants/config.dart
+++ client/lib/constants/config.dart
@@ -3,6 +3,6 @@ class Config {
   static const String registrationUrl = "${url}registration";
   static const String login = '${url}login';
   static const String summarize = '${url}text-summarizer/summarize/';
-  static const String apiKey = 'f920a5d9981e42de91c052c8471db7a2';
+  static const String apiKey = YOUR_NEWS_API_KEY;
   static const String getUserDetails = '${url}getUserDetails';
 }
